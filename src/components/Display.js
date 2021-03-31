b16615b479f8ac951a22dd18e0ad0e7ebdc6ee9c
Giordano Dìaz <diazgiordano@gmail.com>
2021-03-31 10:12:59 -0500

    Add components files and display logic

---
+++ src/components/Display.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+
+export default function Display({ result }) {
+  return (
+    <div>
+      {result}
+    </div>
+  );
+}
+
+Display.propTypes = { result: PropTypes.string };
+Display.defaultProps = { result: '0' };
