1a98ab9d242e04ce0a7e9f2c5100db62e4535e54
Giordano Dìaz <diazgiordano@gmail.com>
2021-04-05 10:27:56 -0500

    Add router and new files to handle home navbar and links

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,21 @@
 import React from 'react';
-import calculate from '../logic/Calculate';
-import ButtonPanel from './ButtonPanel';
-import Display from './Display';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Calculator from './Calculator';
+import Home from './Home';
+import Quote from './Quote';
+import Navbar from './Navbar';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      total: '',
-      next: '',
-      operation: '',
-    };
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(btnName) {
-    const { total, next, operation } = calculate(this.state, btnName);
-    this.setState({ total, next, operation });
-  }
-
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div>
-        <div className="app">
-          <Display result={String(total)} next={String(next)} operation={operation} />
-          <ButtonPanel clickHandler={this.handleClick} />
-        </div>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/quote" component={Quote} />
+          <Route path="/calculator" component={Calculator} />
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 }
