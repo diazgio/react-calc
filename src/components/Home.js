03e548468c95bf4437b0bfe792d3125e08da73c6
Giordano Dìaz <diazgiordano@gmail.com>
2021-04-05 13:05:30 -0500

    Add testing files

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="home">Welcome to our Page!</h1>
-      <p>
+      <p className="lorem">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
         ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
         laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
